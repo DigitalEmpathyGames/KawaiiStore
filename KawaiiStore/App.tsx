6cd53e62ecffca721dfb78f5e3d78e02eae553c2
unknown <digitalempathygames@gmail.com>
2023-03-12 17:12:06 -0300

    menu lateral

--- KawaiiStore/App.tsx
+++ KawaiiStore/App.tsx
@@ -3,16 +3,29 @@ import React from 'react';
 import {
   SafeAreaView,
   Text,
+  View,
 } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+const Drawer = createDrawerNavigator();
 
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>
+        Hola blabla
+      </Text>
+    </View>
+  );
+}
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView>
-      <Text >
-        AppBase
-      </Text>
-    </SafeAreaView>
+    <NavigationContainer>
+          <Drawer.Navigator initialRouteName="Home">
+            <Drawer.Screen name="Home" component={HomeScreen} />
+          </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 

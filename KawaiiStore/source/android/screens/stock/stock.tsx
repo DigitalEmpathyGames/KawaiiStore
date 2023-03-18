7245f49b1a28e378419c10285ec3f473d87ca39c
unknown <digitalempathygames@gmail.com>
2023-03-17 21:30:44 -0300

    moch de pantalla stock

--- KawaiiStore/source/android/screens/stock/stock.tsx
+++ KawaiiStore/source/android/screens/stock/stock.tsx
@@ -3,9 +3,142 @@ import {
     View,
   } from 'react-native';
 
-const Stock = ({ navigation }:any) => {
+type itemStock = {
+  id:string;
+  name:string;
+  price:number;
+  quantity:number;
+}
+
+const Stock = ({ navigation }:any) :JSX.Element => {
+
+    const drawItem = (item :itemStock):JSX.Element => {
+      return (
+        <>
+        <View
+          style={{
+            backgroundColor:'black',
+            width:'90%',
+            height:'10%',
+            flexDirection:'row',
+            borderColor:'black',
+            borderWidth:2
+          }}
+        >
+          <View
+            style={{
+              flex:1,
+              backgroundColor:'white'
+            }}
+          >
+
+          </View>
+          <View
+            style={{
+              flex:3,
+              backgroundColor:'magenta',flexDirection:'column'
+            }}
+          >
+            <View
+              style={{
+                flex:2,
+                borderColor:'black',
+                borderWidth:2
+              }}
+            >
+
+            </View>
+            <View
+              style={{
+                flex:1,
+                borderColor:'black',
+                borderWidth:2,
+                flexDirection:'row'
+              }}
+            >
+                <View
+                  style={{
+                    flex:1,
+                    borderColor:'black',
+                    borderWidth:2
+                  }}
+                >
+
+                </View>
+                <View
+                  style={{
+                    flex:1,
+                    borderColor:'black',
+                    borderWidth:2
+                  }}
+                >
+
+                </View>
+            </View>
+          </View>
+          <View
+            style={{
+              flex:1,
+              backgroundColor:'gray'
+            }}
+          >
+
+          </View>
+        </View>
+        </>
+      )
+    }
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <View style={{ flex: 1, alignItems: 'center',
+          backgroundColor:'red'
+        }}>
+        <View
+          style={{
+            backgroundColor:'black',
+            width:'90%',
+            height:'10%',
+            flexDirection:'row'
+          }}
+        >
+          <View
+            style={{
+              flex:1,
+              backgroundColor:'white'
+            }}
+          >
+
+          </View>
+          <View
+            style={{
+              flex:3,
+              backgroundColor:'magenta'
+            }}
+          >
+
+          </View>
+          <View
+            style={{
+              flex:1,
+              backgroundColor:'gray'
+            }}
+          >
+
+          </View>
+        </View>
+            <View
+              style={
+                {
+                  flex:1,
+                  backgroundColor:'green',
+                  paddingTop:20
+                }
+              }
+            >
+              {drawItem({id:'asdjhgajhsdg',name:'chapitas',price:400,quantity:382})}
+            </View>
+              
+
           <Text>
           Stock
           </Text>
